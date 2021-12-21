4a10738056023717026c7a14e362bb8c25e12ff9
davidHurtado <davidhuertado@gmail.com>
2021-12-21 09:16:41 -0300

    made Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Welcome from './Welcome';
 
 export default () => {
   return (
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './images/logo.svg';
+import menuBars from './images/menu.png';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
       <div className="logo-div">
         <img src={logo} alt="logo" className="logo" />
+      </div>
+      <div className="bars-div">
+        <img src={menuBars} alt="menu" className="menu-bars" />
       </div>
     </div>
   );
